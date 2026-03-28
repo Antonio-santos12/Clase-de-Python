3fdc1629651fcd548833a1839850fc3cb1094bd6
Antonio-santos12 <antoniodelosanto03@gmail.com>
2026-03-28 18:02:19 -0400

    Mi segundo commit

--- Basico/Clase_1.py
+++ Basico/Clase_1.py
@@ -1,1 +1,6 @@
 print("Hola Antonio, bienvenido a la clase de Python")
+
+print("Vamos a aprender a programar en Python")
+print("Python es un lenguaje de programación muy popular y fácil de aprender")
+print("Con Python puedes hacer muchas cosas, como crear sitios web, analizar datos, automatizar")
+
