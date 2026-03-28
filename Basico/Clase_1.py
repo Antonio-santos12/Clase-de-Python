9b6dd64c816b9dbd4ad415ce3010bfdb6fe37b32
Antonio-santos12 <antoniodelosanto03@gmail.com>
2026-03-28 18:10:24 -0400

    Añadir contenido introductorio sobre Python en Clase_1.py

--- Basico/Clase_1.py
+++ Basico/Clase_1.py
@@ -4,3 +4,7 @@ print("Vamos a aprender a programar en Python")
 print("Python es un lenguaje de programación muy popular y fácil de aprender")
 print("Con Python puedes hacer muchas cosas, como crear sitios web, analizar datos, automatizar")
 
+print("tareas, y mucho más")
+print("¡Empecemos a aprender Python juntos!")
+
+print("¿Qué es Python?")
